3f9e68dbec98db4b55af21a42ff5ec2bc2a0d94b
Jon Suderman <suderman@gmail.com>
2013-12-01 02:43:26 -0700

    ajax links, bootstrap

--- index.php
+++ index.php
@@ -5,19 +5,22 @@ $GLOBALS['controllers'] = yaml_parse_file('controllers.yml');
 $GLOBALS['commands'] = yaml_parse_file('commands.yml');
 
 function list_devices(){
-  echo "<h2>API:</h2>";
-  echo "<ul>";
+  echo "<div class=container>";
+  echo "<h1>suder.mn API:</h1>";
+  echo "<dl class=dl-horizontal>";
   foreach($GLOBALS['commands'] as $key => $val) {
-    echo "<li>$key ";
+    echo "<dt>$key</dt>";
+    echo "<dd>";
     list_commands($key);
-    echo "</li>";
+    echo "</dd>";
   }
-  echo "</ul>";
+  echo "</div>";
+  echo "</dl>";
 }
 
 function list_commands($device){
   $host =  $_SERVER["SERVER_NAME"];
-  echo "<ul>";
+  echo "<ul class=list-unstyled>";
   foreach($GLOBALS['commands'][$device] as $key => $val) {
     echo "<li><a href='http://$host/$device/$key'>/$device/$key</a></li>";
   }
@@ -37,6 +40,7 @@ function run_command($device, $command) {
   echo file_get_contents($cmd);
 }
 
+
 # Get the command from the URL
 if (isset($_SERVER["REDIRECT_URL"])) {
   $params = ltrim($_SERVER["REDIRECT_URL"], '/');
@@ -49,6 +53,25 @@ if (isset($_SERVER["REDIRECT_URL"])) {
   }
 
 } else {
+
+?>
+<!DOCTYPE html>
+<html><head>
+<title>suder.mn API</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+<script>
+  $(document).ready(function(){
+    $('a').bind('click',function(event){
+      event.preventDefault();
+      $.get(this.href,{}); 
+    })
+  });
+</script>
+<body>
+<?
 
   list_devices();
 }
